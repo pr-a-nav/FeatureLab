16002f0133175adeae8d14626f17d6d9295cabe2
pr-a-nav <pranavmishra680@gmail.com>
2024-10-17 22:22:05 +0530

    added group

--- Backend/featuremanager.py
+++ Backend/featuremanager.py
@@ -14,3 +14,10 @@ class FeatureManager:
     def __init__(self, db_url) -> None:
         self.engine = sqlalchemy.create_engine(db_url)
         self.Session = sessionmaker(bind = self.engine)
+
+    def create_group(self, name, description):
+        session = self.Session()
+        group = Featuregroup(name,description) # type: ignore
+        session.add(group)
+        session.commit()
+        session.close()
